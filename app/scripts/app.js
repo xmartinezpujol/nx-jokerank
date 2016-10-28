3866ba70500326115da797f211d018e400678fc8
Xavier Martínez <xmartinezpujol@gmail.com>
2016-10-28 05:15:45 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nx_jokerankApp
+ * @description
+ * # nx_jokerankApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nx_jokerankApp', []);
---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('nx_jokerankApp')
+  .controller('MenuCtrl', function () {
+    this.name = 'Nexus - Ranking de chistosos';
+    this.users = [
+     {
+      name: 'Víctor',
+      img: 'srx.png',
+      nickname: 'El veterano',
+      rating: 5
+     },
+     {
+      name: 'Chuichui',
+      img: 'chuichui.png',
+      nickname: 'El veterinario',
+      rating: 4
+     },
+     {
+      name: 'Asorik',
+      img: 'srx.png',
+      nickname: 'El susurrador',
+      rating: 3
+     },
+     {
+      name: 'Keldath',
+      img: 'keldath.png',
+      nickname: 'El malvado',
+      rating: 4
+    },
+     {
+      name: 'Ariane',
+      img: 'ariane.png',
+      nickname: 'La matriarca',
+      rating: 0
+    },
+     {
+      name: 'Sirloss',
+      img: 'sirloss.png',
+      nickname: 'El tontete',
+      rating: 1
+    },
+     {
+      name: 'Deirius',
+      img: 'srx.png',
+      nickname: 'El pastelero',
+      rating: 2
+    }
+    ];
+
+    this.likeUp = function(user){
+      user.rating += 1;
+    }
+    this.likeDown = function(user){
+      user.rating -= 1;
+    }
+  });
